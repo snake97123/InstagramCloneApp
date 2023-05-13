17de85af2b840f71fa6d1603928ad94563c5aacf
snake97123 <73522198+snake97123@users.noreply.github.com>
2023-05-13 09:45:00 +0900

    tabBarの作成

---
+++ Instagram/TabBarController.swift
@@ -0,0 +1,46 @@
+//
+//  TabBarController.swift
+//  Instagram
+//
+//  Created by 山下　航 on 2023/05/13.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController, UITabBarControllerDelegate{
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.tabBar.tintColor = UIColor(red: 1.0, green: 0.44, blue: 0.11, alpha: 1)
+        
+        let appearance = UITabBarAppearance()
+        appearance.backgroundColor = UIColor(red: 0.96, green: 0.91, blue: 0.87, alpha: 1)
+        self.tabBar.standardAppearance = appearance
+        self.tabBar.scrollEdgeAppearance = appearance
+        // Do any additional setup after loading the view.
+        self.delegate = self
+        
+        func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
+            if viewController is ImageSelectViewController {
+                let imageSelectViewController = storyboard!.instantiateViewController(withIdentifier: "ImageSelect")
+                present(imageSelectViewController, animated: true)
+                return false
+            } else {
+                return true
+            }
+        }
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
