2b0fd341e2d8f2d89ab92f4bf8a77e7fd5361a76
snake97123 <73522198+snake97123@users.noreply.github.com>
2023-05-15 11:12:09 +0900

    投稿機能の実装

---
+++ Instagram/Const.swift
@@ -0,0 +1,14 @@
+//
+//  Const.swift
+//  Instagram
+//
+//  Created by 山下　航 on 2023/05/15.
+//
+
+import Foundation
+
+
+struct Const {
+    static let ImagePath = "images"
+    static let PostPath = "posts"
+}
--- Instagram/PostViewController.swift
+++ Instagram/PostViewController.swift
@@ -6,16 +6,58 @@
 //
 
 import UIKit
+import FirebaseAuth
+import FirebaseFirestore
+import FirebaseStorage
+import SVProgressHUD
 
 class PostViewController: UIViewController {
+    var image: UIImage!
 
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        imageView.image = image
     }
     
-
+    @IBAction func handlePostButton(_ sender: Any) {
+        let imageData = image.jpegData(compressionQuality: 0.75)
+        
+        let postRef = Firestore.firestore().collection(Const.PostPath).document()
+        let imageRef = Storage.storage().reference().child(Const.ImagePath).child(postRef.documentID + ".jpg")
+        
+        SVProgressHUD.show()
+        
+        let metadata = StorageMetadata()
+        metadata.contentType = "image/jpeg"
+        imageRef.putData(imageData!, metadata: metadata) { (metadata, error) in
+            if error != nil {
+                print(error!)
+                SVProgressHUD.showError(withStatus: "画像のアップロードが失敗しました。")
+                self.view.window?.rootViewController?.dismiss(animated: true, completion: nil)
+                return
+            }
+            let name = Auth.auth().currentUser?.displayName
+            let postDic = [
+                "name": name!,
+                "caption": self.textField.text!,
+                "date": FieldValue.serverTimestamp(),
+            ] as [String: Any]
+            postRef.setData(postDic)
+            SVProgressHUD.showSuccess(withStatus: "投稿しました。")
+            self.view.window?.rootViewController?.dismiss(animated: true, completion: nil)
+        }
+    }
+    
+    @IBAction func handleCancelButton(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    
     /*
     // MARK: - Navigation
 
